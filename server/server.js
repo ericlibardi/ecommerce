cfaccbc8204cb17d46997f3936c09e48a61cce06
ericlibardi <eric_pereira04@hotmail.com>
2020-08-07 20:52:53 +0100

    first commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require ('express');
+const cors = require('cors');
+const bodyParser =  require('body-parser');
+const connectDB =  require('./config/db');
+
+const app = express();
+
+connectDB();
+
+app.use(express.json({extended: false}))
+app.use(cors());
+
+app.get('/', (req, res) => res.send("API running"))
+
+
+
+const PORT = process.env.PORT || 8080;
